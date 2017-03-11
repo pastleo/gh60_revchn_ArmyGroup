f95a4646cbf6358b0a245803c345b28e0857eef6
PastLeo <chgu82837@gmail.com>
2017-03-12 02:51:58 +0800

    complete my keymap and the readme

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 GH60 RevCHN from ArmyGroup firmware
 =========
 
-Tutorial: [http://blog.dm4.tw/blog/2015/03/17/build-gh60-revchn-on-mac/](http://blog.dm4.tw/blog/2015/03/17/build-gh60-revchn-on-mac/)
+I recently bought a fully customized keyboard from ArmyGroup, after some digging, I found a tutorial about flashing GH60 revCHN here:
 
-Copied and modified from [https://github.com/kairyu/tmk_keyboard_custom/tree/master/keyboard/gh60](https://github.com/kairyu/tmk_keyboard_custom/tree/master/keyboard/gh60)
+[http://blog.dm4.tw/blog/2015/03/17/build-gh60-revchn-on-mac/](http://blog.dm4.tw/blog/2015/03/17/build-gh60-revchn-on-mac/)
 
-## Flashing system requirements
+Thank you man, I did what it says and everything just works, finally I was sure that my keyboard is GH60 revCHN...
 
- * [dfu-programmer](https://dfu-programmer.github.io/)
- * [avr-gcc](http://www.nongnu.org/avr-libc/)
+The code here is copied and modified from [https://github.com/kairyu/tmk_keyboard_custom/tree/master/keyboard/gh60](https://github.com/kairyu/tmk_keyboard_custom/tree/master/keyboard/gh60)
+
+Using [tmk_core_custom](https://github.com/kairyu/tmk_core_custom/tree/927664794a5a24a0fe80640a76f077b4ef74634a) as framework which is a fork (maybe) from [tmk_keyboard](https://github.com/tmk/tmk_keyboard/tree/master/tmk_core)
+
+## System requirements
+
+ * [git](https://git-scm.com/) to get the source code
+ * [avr-gcc](http://www.nongnu.org/avr-libc/) to compile the firmware
+ * [dfu-programmer](https://dfu-programmer.github.io/) to flash into the chip
 
 #### For OSX
 
@@ -20,10 +27,48 @@ brew install dfu-programmer
 
 And follow guide [here](https://github.com/osx-cross/homebrew-avr#installing-homebrew-avr-formulae) to install `avr-gcc`
 
-## Quick Steps:
+#### For Windows
+
+I havn't try out a working way to compile firmware on windows, I use [WSL (with wsltty)](https://github.com/mintty/wsltty) which is a easy way to get a linux with file sharing functionality.
+
+After the firmware is compiled, use [dfu-programmer-win](https://sourceforge.net/projects/dfu-programmer/files/dfu-programmer/):
+
+* Download and unzip somewhere
+* Install driver from `dfu-prog-usb-*/atmel_usb_dfu.inf`, right click on it and choose `Install`
+* Add the `dfu-programmer.exe` to PATH variable.
+* If you can input `dfu-programmer` in console (CMD or powershell) and get no error then you are ready to flash
+
+#### Compile firmware in linux (debian)
+
+```
+sudo apt-get update;
+sudo apt-get install gcc-avr avr-libc make;
+```
+
+And your linux is ready to compile firmware
+
+## Steps:
 
  * `git clone --recursive <this_repo>`
+    * Remember the submodule `tmk_core_custom` must be cloned, donwloading the zip is not enough!
  * `cd <this_repo>`
-    * just make the rom files: `make clean; make`
+    * just compile the firmware: `make clean; make`
+        * compiled firmware is located at `build/gh60revchn_lufa.hex`
     * or make and burn into keyboard: `make clean; make dfu`
+ * flashing: 
+    * Press the button at the back of the keyboard PCB.
+    * `dfu-programmer atmega32u4 get` to check if the keyboard flash device is connected
+    * `dfu-programmer atmega32u4 erase` to erase current firmware
+    * `dfu-programmer atmega32u4 flash build/gh60revchn_lufa.hex` to flash the compiled firmware
+    * `dfu-programmer atmega32u4 reset` to restart your keyboard
+
+## Keymap modifications
+
+tmk keyboard is a great keyboard framework, and it is well-documented too.
+
+I just follow docs [here](https://github.com/kairyu/tmk_core_custom/blob/927664794a5a24a0fe80640a76f077b4ef74634a/doc/keymap.md) and change `keymap_*.c`.
+
+Finally `keymap_pastleo.c` is created with my very own keymap.
+
+The result is not bad with some cost of time.
 
--- keymap_pastleo.c
+++ keymap_pastleo.c
@@ -25,33 +25,40 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP_ANSI(
         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-        LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
+        RCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          FN0,  \
-        FN0 ,LALT,LGUI,          SPC,                     RGUI,RALT,ESC, FN1),
+        LCTL,LALT,LGUI,          SPC,                     RGUI,RALT,ESC, FN1),
     /* 1: windows mode */
     KEYMAP_ANSI(
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS,   \
-        TRNS,LGUI,LALT,          TRNS,                    RGUI,RALT,TRNS,TRNS),
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
+        TRNS,LGUI,LALT,          TRNS,                    RALT,RGUI,TRNS,TRNS),
     /* 2: for non-geeks */
     KEYMAP_ANSI(
         ESC ,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
         CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          RSFT,   \
-        TRNS,LGUI,LALT,          TRNS,                    RALT,RGUI,RCTL,TRNS),
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          RSFT, \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,RCTL,TRNS),
     /* 3: functions */
     KEYMAP_ANSI(
-        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL, \
-        TRNS,TRNS,UP,  PSCR,SLCK,PAUS,WH_L,WH_D,MS_U,WH_U,RSFT,PGUP,INS, CAPS,\
-        TRNS,LEFT,DOWN,RGHT,HOME,END ,WH_R,MS_L,MS_D,MS_R,RCTL,PGDN,     TRNS, \
-        TRNS,BTN1,BTN2,BTN3,MUTE,VOLU,VOLD,MPRV,MPLY,MNXT,APP,           TRNS, \
-        TRNS,TRNS,TRNS,          ACL0,                    TRNS,TRNS,TRNS,TRNS),
-    /* 4: settings */
+        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,  \
+        TRNS,TRNS,UP,  TRNS,HOME,PGUP,TRNS,TRNS,TRNS,TRNS,TRNS,RSFT,INS, CAPS, \
+        TRNS,LEFT,DOWN,RGHT,END ,PGDN,LEFT,DOWN,UP,  RGHT,TRNS,APP,      TRNS, \
+        TRNS,PSCR,SLCK,PAUS,TRNS,MUTE,VOLD,VOLU,MPRV,MPLY,MNXT,          TRNS, \
+        TRNS,TRNS,TRNS,          FN2,                     TRNS,TRNS,TRNS,TRNS),
+    /* 4: mouse mode */
     KEYMAP_ANSI(
-        FN2 ,FN3 ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  , \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,WH_D,MS_U,WH_U,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,MS_L,MS_D,MS_R,TRNS,TRNS,WH_L,WH_D,WH_U,WH_R,TRNS,ACL0,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,BTN1,BTN2,BTN3,          TRNS, \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+    /* 5: settings */
+    KEYMAP_ANSI(
+        FN3 ,FN4 ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  , \
         NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  , \
         NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,     NO  , \
         NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,NO  ,          NO  , \
@@ -60,9 +67,10 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM fn_actions[] = {
     /* Poker Layout */
     [0] = ACTION_LAYER_MOMENTARY(3),  // to Fn overlay
-    [1] = ACTION_LAYER_MOMENTARY(4),  // to settings
-    [2] = ACTION_LAYER_TOGGLE(1),     // mac or windows mode
-    [3] = ACTION_LAYER_TOGGLE(2),     // toggle non-geek mode
+    [1] = ACTION_LAYER_MOMENTARY(5),  // to settings
+    [2] = ACTION_LAYER_MOMENTARY(4),  // to mouse mode
+    [3] = ACTION_LAYER_TOGGLE(1),     // mac or windows mode
+    [4] = ACTION_LAYER_TOGGLE(2),     // toggle non-geek mode
 };
 
 #ifdef KEYMAP_IN_EEPROM_ENABLE
